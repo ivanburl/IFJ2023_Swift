a5cbb5fb30e09b42980928f09f3318dd8324da73
Ivan Burlutskyi <ivandr2004@gmail.com>
2023-10-12 19:45:56 +0200

    scanner implamentation

--- src/scanner/scanner.h
+++ src/scanner/scanner.h
@@ -5,21 +5,31 @@
 #ifndef IFJ2023_SWIFT_SCANNER_H
 #define IFJ2023_SWIFT_SCANNER_H
 
+#include "../model/error/error.h"
 #include "../model/token/token.h"
+#include "../structures/automata/automata.h"
 #include "../structures/vector/vector_t.h"
 #include <limits.h>
 
-vector(Token, Token);
+#define SCANNER_DEFAULT_STATE 0
 
-#define MAX_SCANNER_STATES 64
-
+/**
+ * scanner is used for converting text to list/vector of tokens (prepare for
+ * parsing) start state of automata always should be 0
+ */
 typedef struct scanner {
-  int current_state;
-  int automata[MAX_SCANNER_STATES][CHAR_MAX + 1];
+  Automata automata;
 } Scanner;
 
-void scanner_init(Scanner* scanner);
-void configure_automata(Scanner* scanner);
-TokenVector scan_code(char *text);
+typedef void (*ScannerAutomataConfigurator)(Automata *automata);
+
+void scanner_init(Scanner *scanner, ScannerAutomataConfigurator configurator);
+Error scanner_code_to_tokens(Scanner *scanner, char *code,
+                             TokenVector *tokenVector);
+
+void scanner_configure_swift_2023(Automata *automata);
+void scanner_move_forward(Scanner* scanner, char symbol);
+
+TokenType scanner_get_currentTokenType(Scanner *scanner);
 
 #endif // IFJ2023_SWIFT_SCANNER_H
---
+++ src/scanner/scanner_configs.c
@@ -0,0 +1,11 @@
+#include "scanner.h"
+
+
+void scanner_configure_swift_2023(Automata *automata) {
+  //TODO
+}
+
+
+/*
+ * each terminal should have its oven configuration function, and all of them should be collected into configure automata
+ */
