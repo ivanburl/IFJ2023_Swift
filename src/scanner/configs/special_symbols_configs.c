b1d49faf2967b6e11e1d8cc27e399933cc1eb487
shapo <shaposhnikbogdan@gmail.com>
2023-10-22 14:37:16 +0200

    Scanner Configs SPECIAL_SYMBOLS States Error

--- src/scanner/configs/special_symbols_configs.c
+++ src/scanner/configs/special_symbols_configs.c
@@ -3,22 +3,22 @@
 //TODO do we need " or ' as a quote
 
 void configure_quote(Scanner* scanner){
-  automata_set_edge(&(scanner->automata),0,'\"',53);
-  automata_set_stateReturnValue(&(scanner->automata),53,QUOTE);
+  automata_set_edge(&(scanner->automata),0,'\"',54);
+  automata_set_stateReturnValue(&(scanner->automata),54,QUOTE);
 }
 void configure_left_curl_bracket(Scanner* scanner){
-  automata_set_edge(&(scanner->automata),0,'{',54);
-  automata_set_stateReturnValue(&(scanner->automata),54,LEFT_CURL_BRACKET);
+  automata_set_edge(&(scanner->automata),0,'{',55);
+  automata_set_stateReturnValue(&(scanner->automata),55,LEFT_CURL_BRACKET);
 }
 void configure_right_curl_bracket(Scanner* scanner){
-  automata_set_edge(&(scanner->automata),0,'}',55);
-  automata_set_stateReturnValue(&(scanner->automata),55,RIGHT_CURL_BRACKET);
+  automata_set_edge(&(scanner->automata),0,'}',56);
+  automata_set_stateReturnValue(&(scanner->automata),56,RIGHT_CURL_BRACKET);
 }
 void configure_left_bracket(Scanner* scanner){
-  automata_set_edge(&(scanner->automata),0,'(',56);
-  automata_set_stateReturnValue(&(scanner->automata),56,LEFT_BRACKET);
+  automata_set_edge(&(scanner->automata),0,'(',57);
+  automata_set_stateReturnValue(&(scanner->automata),57,LEFT_BRACKET);
 }
 void configure_right_bracket(Scanner* scanner){
-  automata_set_edge(&(scanner->automata),0,')',57);
-  automata_set_stateReturnValue(&(scanner->automata),57,RIGHT_BRACKET);
+  automata_set_edge(&(scanner->automata),0,')',58);
+  automata_set_stateReturnValue(&(scanner->automata),58,RIGHT_BRACKET);
 }
