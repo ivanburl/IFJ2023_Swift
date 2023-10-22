b783662353c0947526c69b437cccb0ba42fd2274
shapo <shaposhnikbogdan@gmail.com>
2023-10-22 13:59:00 +0200

    Scanner Configs CONDITIONS

---
+++ src/scanner/configs/conditions_configs.c
@@ -0,0 +1,15 @@
+#include "../scanner.h"
+#include"ifj23_swift_configurator.h"
+
+void configure_if(Scanner* scanner){
+  automata_set_edge(&(scanner->automata),0,'i',37);
+  automata_set_edge(&(scanner->automata),37,'f',38);
+  automata_set_stateReturnValue(&(scanner->automata),38,IF);
+}
+void configure_else(Scanner* scanner){
+  automata_set_edge(&(scanner->automata),0,'e',39);
+  automata_set_edge(&(scanner->automata),39,'l',40);
+  automata_set_edge(&(scanner->automata),40,'s',41);
+  automata_set_edge(&(scanner->automata),41,'e',42);
+  automata_set_stateReturnValue(&(scanner->automata),38,ELSE);
+}
