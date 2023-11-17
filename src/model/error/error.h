4722947f25eff396600e056a7c5acc464ee7374d
shapo <shaposhnikbogdan@gmail.com>
2023-11-17 21:12:26 +0100

    Errors update

--- src/model/error/error.h
+++ src/model/error/error.h
@@ -1,12 +1,10 @@
-//
-// Created by burluiva on 10/12/2023.
-//
 
 #ifndef IFJ2023_SWIFT_ERROR_H
 #define IFJ2023_SWIFT_ERROR_H
 
 #include <stdbool.h>
 #include <stdlib.h>
+#include "stdio.h"
 
 #define MAX_ERROR_MESSAGE 256
 
@@ -20,10 +18,12 @@ typedef enum error_exit_code {
 
 typedef struct error_t {
   ErrorType errorType;
+  int errorCode;
   char msg[MAX_ERROR_MESSAGE];
 } Error;
 
 Error error_create(ErrorType errorType, char *msg);
 Error error_unite(Error errorA, Error errorB);
+ErrorType error_report(Error error);
 
 #endif // IFJ2023_SWIFT_ERROR_H
