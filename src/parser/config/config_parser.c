240cfb398a2e543a52fa1de8a388a1d07553f6a9
Ivan Burlutskyi <ivandr2004@gmail.com>
2023-11-22 11:39:38 +0100

    new rules added

--- src/parser/config/config_parser.c
+++ src/parser/config/config_parser.c
@@ -32,11 +32,14 @@ Error ifj_2023_parser_config(Parser *parser) {
   assert(parser->expressionParser && parser->llParser &&
          "Set the ll parser and precedence parser");
 
-  int llGrammarRulesCount = 50;
+  int llGrammarRulesCount = 54;//52
   GrammarRule llGrammarRules[] = {
+      grammar_rule_create(SUPER_DELIMITER, NULL, (TokenType[]){DELIMITER, SUPER_DELIMITER}, 2),
+      grammar_rule_create(SUPER_DELIMITER, NULL, (TokenType[]){0}, 0),
+
       grammar_rule_create(STS, NULL, (TokenType[]){S, STS_TMP}, 2),
       grammar_rule_create(STS, NULL, (TokenType[]){}, 0),
-      grammar_rule_create(STS_TMP, NULL, (TokenType[]){DELIMITER, S, STS_TMP},
+      grammar_rule_create(STS_TMP, NULL, (TokenType[]){DELIMITER, SUPER_DELIMITER, S, STS_TMP},
                           3),
       grammar_rule_create(STS_TMP, NULL, (TokenType[]){}, 0),
       grammar_rule_create(S, NULL, (TokenType[]){WHILE, COND, BLOCK}, 3),
@@ -44,8 +47,8 @@ Error ifj_2023_parser_config(Parser *parser) {
       grammar_rule_create(S, NULL, (TokenType[]){E}, 1),
       grammar_rule_create(S, NULL, (TokenType[]){D}, 1),
       grammar_rule_create(S, NULL, (TokenType[]){RETURN, E}, 2),
-      //TODO S -> continue
-      //TODO s -> break
+      grammar_rule_create(S, NULL, (TokenType[]){CONTINUE}, 1),
+      grammar_rule_create(S, NULL, (TokenType[]){BREAK}, 1),
       grammar_rule_create(COND, NULL, (TokenType[]){E}, 1),
       grammar_rule_create(COND, NULL, (TokenType[]){LET, ID}, 2),
       grammar_rule_create(IF_ELSE, NULL, (TokenType[]){}, 0),
@@ -70,16 +73,16 @@ Error ifj_2023_parser_config(Parser *parser) {
       grammar_rule_create(TANN, NULL, (TokenType[]){COLON, TYPE}, 2),
       grammar_rule_create(TANN_NULL, NULL, (TokenType[]){TANN}, 1),
       grammar_rule_create(TANN_NULL, NULL, (TokenType[]){}, 0),
-      grammar_rule_create(INIT, NULL, (TokenType[]){EQUAL, E}, 2),
+      grammar_rule_create(INIT, NULL, (TokenType[]){ASSIGN, E}, 2),
       grammar_rule_create(INIT, NULL, (TokenType[]){}, 0),
       grammar_rule_create(
-          F, NULL, (TokenType[]){LEFT_BRACKET, UNDERSCORE, RIGHT_BRACKET}, 3),
+          F, NULL, (TokenType[]){LEFT_BRACKET, E, RIGHT_BRACKET}, 3),
       grammar_rule_create(F, NULL, (TokenType[]){ID, F_CALL}, 2),
       grammar_rule_create(F, NULL, (TokenType[]){INTEGER}, 1),
       grammar_rule_create(F, NULL, (TokenType[]){STRING}, 1),
       grammar_rule_create(F, NULL, (TokenType[]){DOUBLE}, 1),
       grammar_rule_create(F, NULL, (TokenType[]){BOOLEAN}, 1),
-      grammar_rule_create(FUNC_ID, NULL, (TokenType[]){HARD_UNWRAP, F}, 2),
+      grammar_rule_create(F, NULL, (TokenType[]){HARD_UNWRAP, F}, 2),
       grammar_rule_create(F_CALL, NULL,
                           (TokenType[]){LEFT_BRACKET, ARGS, RIGHT_BRACKET}, 3),
       grammar_rule_create(F_CALL, NULL, (TokenType[]){}, 0),
@@ -97,17 +100,55 @@ Error ifj_2023_parser_config(Parser *parser) {
       grammar_rule_create(TYPE, NULL, (TokenType[]){STRING_NULLABLE_TYPE}, 1),
       grammar_rule_create(TYPE, NULL, (TokenType[]){DOUBLE_TYPE}, 1),
       grammar_rule_create(TYPE, NULL, (TokenType[]){DOUBLE_NULLABLE_TYPE}, 1),
-      //TODO BOOLEAN
-      //TODO BOOLEAN NULLABLE
+      // TODO BOOLEAN
+      // TODO BOOLEAN NULLABLE
 
+      /// THESE RULES ARE NOT THE PART OF PARSER TREE, ONLY FOR NEXT AND FORWARD
+      /// CALC
+//      grammar_rule_create(E1_TMP, NULL, (TokenType[]){}, 0),
+//      grammar_rule_create(E2_TMP, NULL, (TokenType[]){}, 0),
+//      grammar_rule_create(E3_TMP, NULL, (TokenType[]){}, 0),
+//      grammar_rule_create(E4_TMP, NULL, (TokenType[]){}, 0),
+//      grammar_rule_create(E5_TMP, NULL, (TokenType[]){}, 0),
+//
+//      grammar_rule_create(E, NULL, (TokenType[]){E1, E1_TMP}, 2),
+//      grammar_rule_create(E1_TMP, NULL, (TokenType[]){LOGICAL_OR, E1, E1_TMP},
+//                          3),
+//      grammar_rule_create(E1_TMP, NULL, (TokenType[]){LOGICAL_AND, E1, E1_TMP},
+//                          3),
+//
+//      grammar_rule_create(E1, NULL, (TokenType[]){E2, E2_TMP}, 2),//TODO think about right association
+//      grammar_rule_create(E2_TMP, NULL, (TokenType[]){SOFT_UNWRAP, E2, E2_TMP},
+//                          3),
+//
+//      grammar_rule_create(E2, NULL, (TokenType[]){E3, E3_TMP}, 2),
+//      grammar_rule_create(E3_TMP, NULL, (TokenType[]){EQUAL, E3, E3_TMP}, 3),
+//      grammar_rule_create(E3_TMP, NULL, (TokenType[]){GREATER, E3, E3_TMP}, 3),
+//      grammar_rule_create(E3_TMP, NULL, (TokenType[]){LESS, E3, E3_TMP}, 3),
+//      grammar_rule_create(E3_TMP, NULL,
+//                          (TokenType[]){GREATER_EQUAL, E3, E3_TMP}, 3),
+//      grammar_rule_create(E3_TMP, NULL, (TokenType[]){LESS_EQUAL, E3, E3_TMP},
+//                          3),
+//      grammar_rule_create(E3_TMP, NULL, (TokenType[]){NOT_EQUAL, E3, E3_TMP},
+//                          3),
+//
+//      grammar_rule_create(E3, NULL, (TokenType[]){E4, E4_TMP}, 2),
+//      grammar_rule_create(E4_TMP, NULL, (TokenType[]){PLUS, E4, E4_TMP}, 3),
+//      grammar_rule_create(E4_TMP, NULL, (TokenType[]){MINUS, E4, E4_TMP}, 3),
+//
+//      grammar_rule_create(E4, NULL, (TokenType[]){E5, E5_TMP}, 2),
+//      grammar_rule_create(E5_TMP, NULL, (TokenType[]){MULTIPLY, E5, E5_TMP}, 3),
+//      grammar_rule_create(E4_TMP, NULL, (TokenType[]){DIVIDE, E5, E5_TMP}, 3),
+//
+//      grammar_rule_create(E5, NULL, (TokenType[]){F}, 1),
   };
 
   grammar_configure(parser->llParser->llGrammar, llGrammarRules,
                     llGrammarRulesCount);
 
   ll_parser_configure(parser->llParser);
-  //TODO add boolean operations
-  int pGrammarRulesCount = 11;
+  // TODO add boolean operations
+  int pGrammarRulesCount = 14;
   GrammarRule pGrammarRules[] = {
       grammar_rule_create(E, NULL, (TokenType[]){E, PLUS, E}, 3),
       grammar_rule_create(E, NULL, (TokenType[]){E, MINUS, E}, 3),
@@ -119,20 +160,27 @@ Error ifj_2023_parser_config(Parser *parser) {
       grammar_rule_create(E, NULL, (TokenType[]){E, LESS, E}, 3),
       grammar_rule_create(E, NULL, (TokenType[]){E, GREATER_EQUAL, E}, 3),
       grammar_rule_create(E, NULL, (TokenType[]){E, LESS_EQUAL, E}, 3),
-      grammar_rule_create(E, NULL, (TokenType[]){E, SOFT_UNWRAP, E}, 3)
+      grammar_rule_create(E, NULL, (TokenType[]){E, SOFT_UNWRAP, E}, 3),
+      grammar_rule_create(E, NULL, (TokenType[]){E, LOGICAL_OR, E}, 3),
+      grammar_rule_create(E, NULL, (TokenType[]){E, LOGICAL_AND, E}, 3),
+      grammar_rule_create(E, NULL, (TokenType[]){F}, 1),
   };
 
   grammar_configure(parser->expressionParser->pGrammar, pGrammarRules,
                     pGrammarRulesCount);
   int operatorPriority[MAX_TOKEN_TYPES_NUMBER] = {
-      [MULTIPLY] = 1,      [DIVIDE] = 1,     [MINUS] = 2,      [PLUS] = 2,
-      [EQUAL] = 3,         [NOT_EQUAL] = 3,  [GREATER] = 3,    [LESS] = 3,
-      [GREATER_EQUAL] = 3, [LESS_EQUAL] = 3, [SOFT_UNWRAP] = 4};
+      [MULTIPLY] = 1,   [DIVIDE] = 1,      [MINUS] = 2,
+      [PLUS] = 2,       [EQUAL] = 3,       [NOT_EQUAL] = 3,
+      [GREATER] = 3,    [LESS] = 3,        [GREATER_EQUAL] = 3,
+      [LESS_EQUAL] = 3, [SOFT_UNWRAP] = 4, [LOGICAL_OR] = 5,
+      [LOGICAL_AND] = 5};
   int operatorAssociation[MAX_TOKEN_TYPES_NUMBER] = {
-      [MULTIPLY] = -1,     [DIVIDE] = -1,    [MINUS] = -1,     [PLUS] = -1,
-      [EQUAL] = 0,         [NOT_EQUAL] = 0,  [GREATER] = 0,    [LESS] = 0,
-      [GREATER_EQUAL] = 0, [LESS_EQUAL] = 0, [SOFT_UNWRAP] = 1};
-  precedence_parser_configure(parser->expressionParser, parser->expressionParser->pGrammar,
-                              operatorPriority,
-                              operatorAssociation);
+      [MULTIPLY] = -1,   [DIVIDE] = -1,     [MINUS] = -1,
+      [PLUS] = -1,       [EQUAL] = 0,       [NOT_EQUAL] = 0,
+      [GREATER] = 0,     [LESS] = 0,        [GREATER_EQUAL] = 0,
+      [LESS_EQUAL] = 0,  [SOFT_UNWRAP] = 1, [LOGICAL_OR] = -1,
+      [LOGICAL_AND] = -1};
+  precedence_parser_configure(parser->expressionParser,
+                              parser->expressionParser->pGrammar,
+                              operatorPriority, operatorAssociation);
 }
