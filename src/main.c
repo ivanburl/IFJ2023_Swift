900a48d190a11b2e1750c00014f31f1d841d64ff
Burlustkyi Ivan <ivandr2004@gmail.com>
2023-12-06 22:58:53 +0100

    1

--- src/main.c
+++ src/main.c
@@ -57,7 +57,52 @@ freopen("../IO/output.txt", "w", stdout);
   }
   sem_analyse_start(&sts);
   generate_inter_code(&sts);
+  //exportLLTable(&parser);
   return 0;
 }
 
-
+void exportLLTable(Parser* parser) {
+  char* names[] = {
+    "UNDEFINED", "STRING", "INTEGER",
+      "DOUBLE", "NIL", "MULTILINE_STRING",
+      "COMMENT", "MULTILINE_COMMENT",
+      "BOOLEAN", "ID",
+      "BLANK", "DELIMITER", "STRING_TYPE",
+      "INT_TYPE", "DOUBLE_TYPE", "BOOLEAN_TYPE",
+      "STRING_NULLABLE_TYPE", "INT_NULLABLE_TYPE",
+      "DOUBLE_NULLABLE_TYPE", "BOOLEAN_NULLABLE_TYPE",
+      "IF", "ELSE", "LET", "VAR", "CONTINUE",
+      "BREAK", "WHILE", "LEFT_CURL_BRACKET", "RIGHT_CURL_BRACKET",
+      "LEFT_BRACKET", "RIGHT_BRACKET",
+      "COLON", "SEMICOLON",
+      "PLUS", "MINUS", "MULTIPLY", "DIVIDE",
+      "EQUAL", "ASSIGN", "LESS_EQUAL", "GREATER_EQUAL",
+      "GREATER", "LESS", "NOT_EQUAL",
+      "SOFT_UNWRAP", "HARD_UNWRAP",
+      "FUNC", "ARROW", "COMMA", "RETURN",
+      "UNDERSCORE", "LOGICAL_AND", "LOGICAL_OR",
+      "ID_AND_ASSIGN", "ID_AND_COLON", "VOID_TYPE",
+      "NIL_TYPE", "DOLLAR", "NON_TERMINAL_UNDEFINED",
+      "S", "E", "F", "STS", "STS_TMP",
+      "COND", "BLOCK", "IF_ELSE", "D",
+      "ID_INIT", "PARAM", "FUNC_ID",
+      "PARAMS", "PARAMS_TMP", "TANN", "TYPE",
+      "F_CALL", "ARGS", "ARGS_TMP",
+      "ARG", "FUNC_RETURN", "ID_INIT_ASSIGN",
+      "E_NULLABLE"
+  };
+  printf(" ");
+  for (int j=0;j<NON_TERMINAL_UNDEFINED;j++)
+    printf(",%s", names[j]);
+  printf("\n");
+  for (int i=NON_TERMINAL_UNDEFINED+1;i<=E_NULLABLE;i++) {
+    printf(",%s", names[i]);
+    for (int j = 0; j < NON_TERMINAL_UNDEFINED; j++) {
+      if (parser->llParser->llTable[i][j] == -1)
+        printf(",x");
+      if (parser->llParser->llTable[i][j] != -1)
+        printf(",%d", parser->llParser->llTable[i][j]);
+    }
+    printf("\n");
+  }
+}
